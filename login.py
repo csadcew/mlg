bdf7941b81fa645586bf6b4450c8bdd7a4f75e00
csadcew <2015354265@qq.com>
2025-02-09 21:28:31 +0800

    v2

--- login.py
+++ login.py
@@ -1,11 +1,12 @@
 import streamlit as st
-
+import time
 p = ["96f0a81e13b6d23ecaa4a1db8322a28b", "123"]
 pwd = st.text_input("请输入软件密码：")
 if st.button("登录"):
     if pwd in p:
         st.session_state.login_state = True
         st.info("登录成功")
+        time.sleep(0.5)
         st.rerun()
     else:
         st.session_state.login_state = False
